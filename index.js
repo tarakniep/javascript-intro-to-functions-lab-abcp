73c7457b6ed24371f5764b3a7a2df35731a0362e
tara kniep <tara.kniep@gmail.com>
2018-01-19 01:11:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ function whisper(string){
 
 function logShout(string){
   console.log('HELLO')
-  return string('YES INDEED')
+  return string('YES INDEED!')
 }
 
 function logWhisper(string){
   console.log('hello')
+  return string('I can\'t hear you!')
+}
+
+function sayHiToGrandma(string){
+  console.log('I love you, Grandma.')
+  return string('I love you, too.')
 }
