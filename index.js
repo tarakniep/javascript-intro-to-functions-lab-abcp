aa63900a39e98714b6ca5ae88460b9013bc3fff4
tara kniep <tara.kniep@gmail.com>
2018-01-19 01:21:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,14 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toUpperCase === "HELLO")
-    
+  if (string === "HELLO"){
+    return ('YES INDEED!')
   }
-}
+  
+  else if (string ==="hello"){
+    return ('I can\'t hear you!')
+  }
+  else if (string === "I love you, Grandma."){
+    return ('I love you, too.')
+  }
+  }
